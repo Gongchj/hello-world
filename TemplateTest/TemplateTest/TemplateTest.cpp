05baf79ba08c3a65bfb7fcf880b44268cd4be1b6
Marfox <SwaggyTyrion@gmail.com>
2017-01-25 15:00:41 -0800

    add MyTemplate

---
+++ TemplateTest/TemplateTest/TemplateTest.cpp
@@ -0,0 +1,14 @@
+// TemplateTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include "MyTemplate.h"
+int main()
+{
+	double m = 2.0;
+	double n = 3.0;
+	double p = Plus(m, n);
+    return 0;
+}
+
